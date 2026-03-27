5a4ed9f51abe8da94e048e06ba6d0f98fb0c121b
stanley <stanleyonahu@gmail.com>
2026-03-27 13:18:24 +0100

    add route to get company by state

--- server/api/companies/company.routes.js
+++ server/api/companies/company.routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { registerCompany } from './company.controller.js';
+import { registerCompany, getCompaniesByState } from './company.controller.js';
 import { uploadLogo } from '../../middleware/upload.middleware.js';
 
 const router = express.Router();
 
 router.post('/register', uploadLogo.single('logo'), registerCompany);
+router.get('/state/:state', getCompaniesByState);
 
 export default router;
