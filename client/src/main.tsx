d7709375934da31bd55789b8d75a792e5528e026
Katchy1942 <stanleyonahu@gmail.com>
2026-04-12 17:48:42 +0100

    add vercel analytics

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,10 +2,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { Toaster } from "@/components/ui/sonner"
+import { Analytics } from '@vercel/analytics/react';
 
 createRoot(document.getElementById('root')!).render(
 	<>
 	<App />
+	<Analytics />
 	<Toaster
 		richColors
 		position='top-center'
