3a0d715c33d29016589db58a5180129707c9c01c
Katchy1942 <stanleyonahu@gmail.com>
2026-08-25 18:45:55 +0100

    update database params for deployment

--- server/config/config.cjs
+++ server/config/config.cjs
@@ -1,16 +1,22 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
 	development: {
 		url: process.env.DATABASE_URL,
-		dialect: 'postgres',
+		dialect: "postgres",
 	},
 	test: {
 		url: process.env.DATABASE_URL,
-		dialect: 'postgres',
+		dialect: "postgres",
 	},
 	production: {
 		url: process.env.DATABASE_URL,
-		dialect: 'postgres',
+		dialect: "postgres",
+		dialectOptions: {
+			ssl: {
+				require: true,
+				rejectUnauthorized: false,
+			},
+		},
 	},
 };
--- server/config/database.js
+++ server/config/database.js
@@ -5,6 +5,14 @@ dotenv.config();
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
 	dialect: 'postgres',
 	logging: false,
+	...(process.env.NODE_ENV === 'production' && {
+		dialectOptions: {
+			ssl: {
+				require: true,
+				rejectUnauthorized: false,
+			},
+		},
+	}),
 });
 
 export default sequelize;
