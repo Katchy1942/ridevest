eb498f2d7837ed5d3a0c792c1cd897a1bc50845d
Katchy1942 <stanleyonahu@gmail.com>
2026-08-26 08:35:04 +0100

    UI updates

--- client/src/components/common/Header.tsx
+++ client/src/components/common/Header.tsx
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom";
 import { ArrowRight, SquareKanban } from "lucide-react";
 
-
 const Header = () => {
 	const isSignedIn = !!localStorage.getItem("token");
 
@@ -20,36 +19,15 @@ const Header = () => {
 				ridevest
 			</Link>
 
-			{/* <nav
-				className="hidden sm:flex items-center gap-6 bg-white/10 
-					backdrop-blur-md border border-white/20 rounded-full 
-					px-6 py-2 shadow-lg absolute left-1/2 -translate-x-1/2"
-			>
-				{navLinks.map(({ label, to }) => (
-					<NavLink
-						key={to}
-						to={to}
-						className={({ isActive }) =>
-							`text-sm transition-colors whitespace-nowrap ${
-								isActive
-									? "text-white font-semibold"
-									: "text-white/60 hover:text-white"
-							}`
-						}
-					>
-						{label}
-					</NavLink>
-				))}
-			</nav> */}
-
 			<div className="flex items-center justify-center gap-2">
 				{isSignedIn ? (
 					<Link
 						to="/dashboard"
-						className="shrink-0 flex items-center gap-1.5 bg-emerald-600 hover:bg-emerald-700
-						text-black text-xs sm:text-sm px-3 py-1.5 
-						sm:px-4 sm:py-1.5 rounded-full transition-colors 
-						cursor-pointer whitespace-nowrap tracking-tight"
+						className="shrink-0 flex items-center
+							gap-1.5 bg-emerald-600 hover:bg-emerald-700
+							text-black text-xs sm:text-sm px-3 py-1.5 
+							sm:px-4 sm:py-1.5 rounded-full transition-colors 
+							cursor-pointer whitespace-nowrap tracking-tight"
 					>
 						<SquareKanban size={14} strokeWidth={1.5} />
 						Dashboard
@@ -58,21 +36,26 @@ const Header = () => {
 					<>
 						<Link
 							to="/login"
-							className="shrink-0 flex flex-row-reverse items-center gap-1 bg-emerald-600 hover:bg-emerald-700
-							text-black text-xs sm:text-sm px-3 py-1.5 
-							sm:px-4 sm:py-1.5 rounded-full transition-colors 
-							cursor-pointer whitespace-nowrap tracking-tight"
+							className="shrink-0 flex flex-row-reverse 
+								items-center gap-1 bg-emerald-600 hover:bg-emerald-700
+								text-black text-xs sm:text-sm px-3 py-1.5 
+								sm:px-4 sm:py-1.5 rounded-full transition-colors 
+								cursor-pointer whitespace-nowrap tracking-tight"
 						>
 							<ArrowRight size={14} strokeWidth={1.5} />
 							Login
 						</Link>
 						<Link
 							to="/register"
-							className="shrink-0 flex items-center gap-1 text-emerald-600 hover:text-emerald-700 text-sm
-							transition-colors 
-							cursor-pointer whitespace-nowrap tracking-tight"
+							className="shrink-0 flex items-center gap-1 
+								text-emerald-600 hover:text-emerald-700 text-sm
+								transition-colors 
+								cursor-pointer whitespace-nowrap tracking-tight"
 						>
-							Register your company
+							<span className="sm:hidden">Register</span>
+							<span className="hidden sm:inline">
+								Register your company
+							</span>
 						</Link>
 					</>
 				)}
