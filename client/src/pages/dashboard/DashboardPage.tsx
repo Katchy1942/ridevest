690442a058f560e95ced906f904ef8c602bfc350
Katchy1942 <stanleyonahu@gmail.com>
2026-08-29 12:58:33 +0100

    update app link

--- client/src/pages/dashboard/DashboardPage.tsx
+++ client/src/pages/dashboard/DashboardPage.tsx
@@ -3,7 +3,7 @@ const DashboardPage = () => {
    const company = companyData ? JSON.parse(companyData) : null
    const companyName = company?.companyName
    return (
-      <div className="text-sm">To test order creation and tracking: <a href={`http://localhost:5173/add-delivery?company=${companyName?.replace(/\s+/g, '-')}`} className="text-emerald-500">Create Order</a></div>
+      <div className="text-sm">To test order creation and tracking: <a href={`https://ridevest.name.ng/add-delivery?company=${companyName?.replace(/\s+/g, '-')}`} className="text-emerald-500">Create Order</a></div>
    )
 }
 
